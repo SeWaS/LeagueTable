da91a06a6b808088da0f0ed6478635b4c5d8a61c
SeWa <sebastian_walter@me.com>
2017-06-30 18:12:54 +0200

    Updated Group POJO to not have public properties

--- src/main/java/org/sewas/domain/model/Group.java
+++ src/main/java/org/sewas/domain/model/Group.java
@@ -1,5 +1,6 @@
 package org.sewas.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -9,15 +10,40 @@ import java.io.Serializable;
  * Created by sebastian on 21/05/17.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonAutoDetect(fieldVisibility=JsonAutoDetect.Visibility.ANY, getterVisibility=JsonAutoDetect.Visibility.NONE,
+        setterVisibility=JsonAutoDetect.Visibility.NONE, creatorVisibility=JsonAutoDetect.Visibility.NONE)
 public class Group {
 
     @JsonProperty("GroupID")
-    public int GroupID;
+    private int GroupID;
 
     @JsonProperty("GroupName")
-    public String GroupName;
+    private String GroupName;
 
     @JsonProperty("GroupOrderID")
-    public int GroupOrderID;
+    private int GroupOrderID;
 
+    public int getGroupID() {
+        return GroupID;
+    }
+
+    public void setGroupID(int groupID) {
+        GroupID = groupID;
+    }
+
+    public String getGroupName() {
+        return GroupName;
+    }
+
+    public void setGroupName(String groupName) {
+        GroupName = groupName;
+    }
+
+    public int getGroupOrderID() {
+        return GroupOrderID;
+    }
+
+    public void setGroupOrderID(int groupOrderID) {
+        GroupOrderID = groupOrderID;
+    }
 }
