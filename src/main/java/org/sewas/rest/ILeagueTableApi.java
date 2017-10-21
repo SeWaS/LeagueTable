7e410f692d3832f45c94770aec75d869edc8f238
SeWa <sebastian_walter@me.com>
2017-10-21 10:30:41 +0200

    Added jQuery-ui for accordion support

--- src/main/java/org/sewas/rest/ILeagueTableApi.java
+++ src/main/java/org/sewas/rest/ILeagueTableApi.java
@@ -9,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.awt.*;
+
 /**
  * Created by sebastian on 21/05/17.
  */
